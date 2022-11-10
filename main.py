933bbed75b03d78d7f795569fb67de0970dc10d9
dmastrih <dmastrih@gmail.com>
2022-11-10 19:04:45 +0300

    create class Pouchs

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
+import random
+
+
 class Player:
     pass
 
 
 class Card:
     pass
+
+
+class Barrel:
+    pass
+
+
+class Pouchs:
+    def __init__(self):
+        self.count = 90
+        self._list_pouchs = []
+
+    def get_pouch(self):
+        current_pouch = random.randrange(1, 4)
+
+        if self._list_pouchs:
+            while current_pouch in self._list_pouchs:
+                current_pouch = random.randrange(1, 4)
+
+        self._list_pouchs.append(current_pouch)
+        return current_pouch
+
+    def quantity_info(self):
+        self.count -= 1
+        return self.count
+
+
+if __name__ == '__main__':
 
